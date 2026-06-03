871fa85424cc444a55c464e2bd727fdeb90da11f
Md. Abu Rayhan <shuvoabu4321@gmail.com>
2026-06-03 15:49:33 +0600

    Optimize stream for 1080p 25fps

--- app/components/JitsiRoom.js
+++ app/components/JitsiRoom.js
@@ -66,8 +66,14 @@ export default function JitsiRoom({ role = "viewer", room }) {
             disableInviteFunctions: true,
             disableTileView: !isHost,
             disableSelfView: !isHost,
-            desktopSharingFrameRate: { min: 30, max: 30 },
-            resolution: 720,
+            desktopSharingFrameRate: { min: 25, max: 25 },
+            resolution: 1080,
+            constraints: {
+              video: {
+                height: { ideal: 1080, max: 1080, min: 720 },
+                frameRate: { ideal: 25, max: 25, min: 25 }
+              }
+            },
             filmstrip: { disabled: !isHost },
             hideConferenceSubject: true,
             hideConferenceTimer: true,
