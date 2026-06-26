bd2508a3f65cb08f481d45efd13ce8c26d73a784
Md. Abu Rayhan <shuvoabu4321@gmail.com>
2026-06-26 21:15:55 +0600

    cleanup: remove offline/non-functional tv channels

--- app/data/tvChannels.js
+++ app/data/tvChannels.js
@@ -13,16 +13,6 @@ export const TV_CHANNELS = [
     no_proxy: true
   },
   {
-    name: "M6 Direct TV",
-    logo: "https://i.imgur.com/7GVp3fW.png",
-    group: "FIFA",
-    url: "https://origin-m6web.live.6cloud.fr/out/v1/6play/6play-m6/cmaf_cenc00/dash-short-hd.mpd",
-    type: "dash",
-    kid: "433ffba670963e70857859a9dff4be04",
-    key: "51ede3a821229fe81e71282c8eff80e3",
-    no_proxy: true
-  },
-  {
     name: "DAZN FIFA",
     logo: "https://i.postimg.cc/Kc3mP7Qt/cbimage.png",
     group: "FIFA",
@@ -39,17 +29,6 @@ export const TV_CHANNELS = [
     no_proxy: true
   },
   {
-    name: "FOX",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Fox_Broadcasting_Company_logo_%282019%29.svg/250px-Fox_Broadcasting_Company_logo_%282019%29.svg.png",
-    group: "FIFA",
-    url: "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/9crxia693u/out/v1/fc40f22f10374517a2784e1d97cb23f4/cenc.mpd",
-    type: "dash",
-    kid: "1f68713028d439ec03be07f56c1d6213",
-    key: "20093db6455160fffed4c394def3193d",
-    no_proxy: true
-  },
-
-  {
     name: "Win Sports",
     logo: "https://images.seeklogo.com/logo-png/25/1/win-sports-logo-png_seeklogo-259337.png",
     group: "FIFA",
@@ -63,31 +42,7 @@ export const TV_CHANNELS = [
     group: "FIFA",
     url: "https://dfr80qz435crc.cloudfront.net/MNOP/Amagi/Caze/Caze_TV_BR/Caze_TV.m3u8",
     type: "hls"
-  },
-  {
-    name: "Somoy TV",
-    logo: "https://s3.aynaott.com/storage/ece71c1163a377fbe2d93f9d28c34f60",
-    group: "FIFA",
-    url: "https://live.thebosstv.com:30443/dwlive/Somoy-TV/chunks.m3u8",
-    type: "hls",
-    no_proxy: true
-  },
-  {
-    name: "TVP Sport",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Logo_TVP_Sport.jpg/1280px-Logo_TVP_Sport.jpg",
-    group: "FIFA",
-    url: "https://1nyaler.streamhostingcdn.top/stream/89/index.m3u8",
-    type: "hls",
-    no_proxy: true
-  },
-  {
-    name: "TSN 1",
-    logo: "https://images.seeklogo.com/logo-png/31/1/tsn-1-logo-png_seeklogo-314693.png",
-    group: "FIFA",
-    url: "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/w0rehjjrwe/out/v1/69a2a7041395406b970598f61680e7cf/cenc.mpd",
-    type: "dash",
-    kid: "14eeabf30c14b7fbf3008c03099ce011",
-    key: "17d2ac8dbc5429bd70af3433aa12158d",
-    no_proxy: true
   }
+
+
 ];
