6554e32285e3e392d270acf691d269011cec7173
Md. Abu Rayhan <shuvoabu4321@gmail.com>
2026-06-29 19:00:19 +0600

    Update TV channels list: add D Sports FHD, M6 USA, FOX ONE AQ and clean list

--- app/data/tvChannels.js
+++ app/data/tvChannels.js
@@ -21,16 +21,6 @@ export const TV_CHANNELS = [
     no_proxy: true
   },
   {
-    name: "FOX SPORTS 4K",
-    logo: "https://images.seeklogo.com/logo-png/28/1/fox-sports-logo-png_seeklogo-284763.png",
-    group: "FOX",
-    url: "https://otte.cache.aiv-cdn.net/lhr-nitro/live/clients/dash/enc/m5hvr8vyu9/out/v1/31d30c91fc65458789b84209d3fa22e4/cenc.mpd",
-    type: "dash",
-    kid: "1f68713028d439ec03be07f56c1d6213",
-    key: "20093db6455160fffed4c394def3193d",
-    no_proxy: false
-  },
-  {
     name: "TSN SPORTS FHD",
     logo: "https://www.bellmedia.ca/lede/wp-content/uploads/2024/09/18581592_10155403529061055_8240563011649656197_n.jpg",
     group: "TSN",
@@ -51,7 +41,27 @@ export const TV_CHANNELS = [
     key: "a879b1d38ed002d4018bce96f9219b8d",
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36",
     no_proxy: false
+  },
+  {
+    name: "FOX ONE - AQ",
+    logo: "https://images.seeklogo.com/logo-png/28/1/fox-sports-logo-png_seeklogo-284763.png",
+    group: "FOX",
+    url: "https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/enc/ajfoeddkbz/out/v1/b78800b9b2304879b15843f455836829/cenc.mpd",
+    type: "dash",
+    kid: "f6564ec2aee819046328a0e153be574d",
+    key: "ff46a8a1031eb27ef22576a077c98ab7",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36",
+    no_proxy: false
+  },
+  {
+    name: "D Sports FHD",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/5/5a/DSports.png",
+    group: "D",
+    url: "https://otte.cache.aiv-cdn.net/iad-nitro/live/clients/dash/enc/z5oyxzsxdk/out/v1/7695a0f64a0e424b973d5b09a2a3eb91/cenc.mpd",
+    type: "dash",
+    kid: "f836853d8eac19446ed9535f5fc568b1",
+    key: "b3bc5ef00602b29abac7e482d3d9fbf3",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36",
+    no_proxy: false
   }
 ];
-
-
